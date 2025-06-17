232fcf282608391e82f1dbd1b14c08a38c955e01
Rojonantenaina-RAK <rojo.rak39@gmail.com>
2025-06-17 07:08:16 -0500

    style: stylize the root / page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,19 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <Link href="/login">Login</Link>
-      <Link href="/register">Register</Link>
+    <div className="flex justify-evenly mt-[45vh] font-[family-name:var(--font-geist-sans)]">
+      <Link
+        className="bg-green-500 text-white p-3 rounded w-30 text-center"
+        href="/login"
+      >
+        Login
+      </Link>
+      <Link
+        className="bg-blue-500 text-white p-3 rounded w-30 text-center"
+        href="/register"
+      >
+        Register
+      </Link>
     </div>
   );
 }
